e5d2fc857665df4bd81bd52065e7a2a076849b07
Kunho Park <github@2odk.com>
2026-07-12 16:29:30 +0900

    release: v0.4.6

--- engine/src/moru_engine/__init__.py
+++ engine/src/moru_engine/__init__.py
@@ -23,7 +23,7 @@ from .placeholder import PlaceholderError, PlaceholderProtector
 from .scanner import ModpackScanner, ScanResult, scan_modpack
 from .validator import TranslationValidator
 
-__version__ = "0.4.5"
+__version__ = "0.4.6"
 
 __all__ = [
     "BatchTranslator",
